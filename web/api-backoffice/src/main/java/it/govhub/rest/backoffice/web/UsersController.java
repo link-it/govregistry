88a0f81ba0566dbcfc7ec733df9a3bf9e48e666a
fscarlato <f.scarlato@protonmail.com>
2022-11-02 15:06:30 +0100

    Completata scrittura OpenAPI per le utenze ad eccezione della lista paginata restituita dalla listUsers

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
@@ -1,14 +1,39 @@
 package it.govhub.rest.backoffice.web;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 
 import it.govhub.management_bo.api.UsersApi;
+import it.govhub.management_bo.beans.PatchOp;
 import it.govhub.management_bo.beans.User;
+import it.govhub.management_bo.beans.UserCreate;
+import it.govhub.management_bo.beans.UserOrdering;
 
 public class UsersController implements UsersApi {
 
+
 	@Override
-	public ResponseEntity<User> createUser(User body) {
+	public ResponseEntity<User> createUser(UserCreate userCreate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<User> readUser(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Object> listUsers(Integer limit,	Long offset,	String q, Boolean enabled, UserOrdering orderBy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<User> updateUser(List<PatchOp> patchOp) {
 		// TODO Auto-generated method stub
 		return null;
 	}
