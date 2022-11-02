2b05708a047372ac57a7c49935c41ff621b7e2b2
fscarlato <f.scarlato@protonmail.com>
2022-11-02 15:27:03 +0100

    Completata la scrittura OpenAPI per le utenze

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
@@ -9,6 +9,7 @@ import it.govhub.management_bo.api.UsersApi;
 import it.govhub.management_bo.beans.PatchOp;
 import it.govhub.management_bo.beans.User;
 import it.govhub.management_bo.beans.UserCreate;
+import it.govhub.management_bo.beans.UserList;
 import it.govhub.management_bo.beans.UserOrdering;
 
 public class UsersController implements UsersApi {
@@ -27,7 +28,7 @@ public class UsersController implements UsersApi {
 	}
 
 	@Override
-	public ResponseEntity<Object> listUsers(Integer limit,	Long offset,	String q, Boolean enabled, UserOrdering orderBy) {
+	public ResponseEntity<UserList> listUsers(Integer limit,	Long offset,	String q, Boolean enabled, UserOrdering orderBy) {
 		// TODO Auto-generated method stub
 		return null;
 	}
