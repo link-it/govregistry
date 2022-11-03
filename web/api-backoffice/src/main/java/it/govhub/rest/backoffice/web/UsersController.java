ad57ae64009922154422b4b1b748deed339a59f3
Lorenzo Nardi <nardi@link.it>
2022-11-03 14:20:53 +0100

    Aggiunta implementazione dummy delle api users

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.govhub.management_bo.api.UsersApi;
 import it.govhub.management_bo.beans.PatchOp;
@@ -12,33 +13,27 @@ import it.govhub.management_bo.beans.UserCreate;
 import it.govhub.management_bo.beans.UserList;
 import it.govhub.management_bo.beans.UserOrdering;
 
+@RestController
 public class UsersController implements UsersApi {
-
 
 	@Override
 	public ResponseEntity<User> createUser(UserCreate userCreate) {
-		// TODO Auto-generated method stub
-		return null;
+		return ResponseEntity.ok(new User());
 	}
 
 	@Override
 	public ResponseEntity<User> readUser(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return ResponseEntity.ok(new User());
 	}
 
 	@Override
-	public ResponseEntity<UserList> listUsers(Integer limit,	Long offset,	String q, Boolean enabled, UserOrdering orderBy) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<UserList> listUsers(Integer limit, Long offset, String q, Boolean enabled, UserOrdering orderBy) {
+		return ResponseEntity.ok(new UserList());
 	}
 
 	@Override
 	public ResponseEntity<User> updateUser(List<PatchOp> patchOp) {
-		// TODO Auto-generated method stub
-		return null;
+		return ResponseEntity.ok(new User());
 	}
-
-
 
 }
