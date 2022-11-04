401c1cb57d50753a3d0b08016d93659c1224ca5b
fscarlato <f.scarlato@protonmail.com>
2022-11-04 19:30:31 +0100

    wip userService. Mappato il messaggio di errore di validazione in qualcosa di più leggibile rispetto al messaggio di default

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/services/UserService.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/services/UserService.java
@@ -1,9 +1,39 @@
 package it.govhub.rest.backoffice.services;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.govhub.rest.backoffice.beans.UserCreate;
+import it.govhub.rest.backoffice.entity.UserEntity;
+import it.govhub.rest.backoffice.exception.ConflictException;
+import it.govhub.rest.backoffice.repository.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepo;
 
+	@Transactional
+	public UserEntity createUser(UserCreate userCreate) {
+		
+		Optional<UserEntity> found = this.userRepo.findByPrincipal(userCreate.getPrincipal());
+		if (found.isPresent()) {
+			throw new ConflictException("User with principal ["+userCreate.getPrincipal()+"] already exists");
+		}
+		
+		UserEntity newUser = UserEntity.builder()
+				.principal(userCreate.getPrincipal())
+				.full_name(userCreate.getFullName())
+				.enabled(userCreate.isEnabled())
+				.email(userCreate.getEmail())
+				.build();
+		
+		return this.userRepo.save(newUser);
+	}
 
 }
--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
@@ -1,10 +1,6 @@
 package it.govhub.rest.backoffice.web;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +14,6 @@ import it.govhub.rest.backoffice.beans.UserCreate;
 import it.govhub.rest.backoffice.beans.UserList;
 import it.govhub.rest.backoffice.beans.UserOrdering;
 import it.govhub.rest.backoffice.entity.UserEntity;
-import it.govhub.rest.backoffice.exception.ConflictException;
 import it.govhub.rest.backoffice.exception.ResourceNotFoundException;
 import it.govhub.rest.backoffice.repository.UserRepository;
 import it.govhub.rest.backoffice.services.UserService;
@@ -53,30 +48,18 @@ public class UsersController implements UsersApi {
 		return ResponseEntity.ok(new UserList());
 	}
 
+
 	@Override
 	public ResponseEntity<User> updateUser(List<PatchOp> patchOp) {
 		return ResponseEntity.ok(new User());
 	}
 
+	
 	@Override
-	public ResponseEntity<User> createUser(@Valid UserCreate userCreate) {
-		
-		Optional<UserEntity> found = this.userRepo.findByPrincipal(userCreate.getPrincipal());
-		if (found.isPresent()) {
-			throw new ConflictException("User with principal ["+userCreate.getPrincipal()+"] already exists");
-		}
-		
-		UserEntity newUser = UserEntity.builder()
-				.principal(userCreate.getPrincipal())
-				.full_name(userCreate.getFullName())
-				.enabled(userCreate.isEnabled())
-				.email(userCreate.getEmail())
-				.build();
-		
-		newUser = this.userRepo.save(newUser);
-		
-		User ret = this.userAssembler.toModel(newUser);		
-		
+	public ResponseEntity<User> createUser(UserCreate userCreate) {
+		UserEntity newUser = this.userService.createUser(userCreate);
+
+		User ret = this.userAssembler.toModel(newUser);
 		return ResponseEntity.ok(ret);
 	}
 
