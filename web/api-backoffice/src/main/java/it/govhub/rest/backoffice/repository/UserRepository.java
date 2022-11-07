d00668a2a3cd31078ed503507489fc646e8fbf9f
fscarlato <f.scarlato@protonmail.com>
2022-11-08 00:29:52 +0100

    - Scritta implementazione generica per una utility che genera le liste paginate da restituire in REST e mappa offset e limit 	  in pagine. E' stata semplificata e riadattata quella originaria del portale.
    	- Aggiunto generatore metamodel hibernate durante la fase di generate-sources per fare query tipate con i media query.
    
    	- Completata la listUsers
    
    	- Aggiunto mapping per MethodArgumentTypeMismatchException su BAD_REQUEST

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/repository/UserRepository.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/repository/UserRepository.java
@@ -2,11 +2,14 @@ package it.govhub.rest.backoffice.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
 import it.govhub.rest.backoffice.entity.UserEntity;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
+public interface UserRepository extends JpaRepositoryImplementation<UserEntity, Long>{
 	
 	public Optional<UserEntity> findByPrincipal(String principal);
+	
+	
 
 }
--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UsersController.java
@@ -2,11 +2,17 @@ package it.govhub.rest.backoffice.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -15,6 +21,7 @@ import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 
 import it.govhub.rest.backoffice.api.UsersApi;
+import it.govhub.rest.backoffice.assemblers.LimitOffsetPageRequest;
 import it.govhub.rest.backoffice.assemblers.UserAssembler;
 import it.govhub.rest.backoffice.beans.PatchOp;
 import it.govhub.rest.backoffice.beans.User;
@@ -24,8 +31,10 @@ import it.govhub.rest.backoffice.beans.UserOrdering;
 import it.govhub.rest.backoffice.entity.UserEntity;
 import it.govhub.rest.backoffice.exception.BadRequestException;
 import it.govhub.rest.backoffice.exception.ResourceNotFoundException;
+import it.govhub.rest.backoffice.repository.UserFilters;
 import it.govhub.rest.backoffice.repository.UserRepository;
 import it.govhub.rest.backoffice.services.UserService;
+import it.govhub.rest.backoffice.utils.ListaUtils;
 import it.govhub.rest.backoffice.utils.PostgreSQLUtilities;
 import it.govhub.rest.backoffice.utils.RequestUtils;
 
@@ -60,9 +69,31 @@ public class UsersController implements UsersApi {
 		return ResponseEntity.ok(ret);
 	}
 
+	
 	@Override
 	public ResponseEntity<UserList> listUsers(Integer limit, Long offset, String q, Boolean enabled, UserOrdering orderBy) {
-		return ResponseEntity.ok(new UserList());
+
+		LimitOffsetPageRequest pageRequest = new LimitOffsetPageRequest(offset, limit);
+		
+		Specification<UserEntity> spec = UserFilters.empty();
+		if (q != null) {
+			spec = UserFilters.likeFullName(q).or(UserFilters.likePrincipal(q));
+		}
+		if (enabled != null) {
+			spec = spec.and(UserFilters.byEnabled(enabled));
+		}
+		
+		Page<UserEntity> users = this.userRepo.findAll(spec, pageRequest.pageable);
+		
+		HttpServletRequest curRequest = ((ServletRequestAttributes) RequestContextHolder
+				.currentRequestAttributes()).getRequest();
+		
+		UserList ret = ListaUtils.costruisciListaPaginata(users, curRequest, new UserList());
+		for (UserEntity user : users) {
+			ret.addItemsItem(this.userAssembler.toModel(user));
+		}
+		
+		return ResponseEntity.ok(ret);
 	}
 
 	
@@ -109,7 +140,7 @@ public class UsersController implements UsersApi {
 		// Faccio partire la validazione custom per la stringa \u0000
 		PostgreSQLUtilities.throwIfContainsNullByte(updatedContact.getFullName(), "full_name");
 		
-		// Dall'oggetto passo alla entity
+		// Dall'oggetto REST passo alla entity
 		UserEntity newUser = UserEntity.builder()
 				.email(updatedContact.getEmail())
 				.enabled(updatedContact.isEnabled())
