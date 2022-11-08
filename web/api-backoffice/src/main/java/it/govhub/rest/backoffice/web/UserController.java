00686989fca118b6fccb7735fb30acfa23c6c413
fscarlato <f.scarlato@protonmail.com>
2022-11-08 15:24:20 +0100

    Refactoring patchUsers, resa transazionale.

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UserController.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/web/UserController.java
@@ -8,17 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
-import com.github.fge.jsonpatch.JsonPatchException;
 
 import it.govhub.rest.backoffice.api.UsersApi;
 import it.govhub.rest.backoffice.assemblers.LimitOffsetPageRequest;
@@ -29,13 +23,11 @@ import it.govhub.rest.backoffice.beans.UserCreate;
 import it.govhub.rest.backoffice.beans.UserList;
 import it.govhub.rest.backoffice.beans.UserOrdering;
 import it.govhub.rest.backoffice.entity.UserEntity;
-import it.govhub.rest.backoffice.exception.BadRequestException;
 import it.govhub.rest.backoffice.exception.ResourceNotFoundException;
 import it.govhub.rest.backoffice.repository.UserFilters;
 import it.govhub.rest.backoffice.repository.UserRepository;
 import it.govhub.rest.backoffice.services.UserService;
 import it.govhub.rest.backoffice.utils.ListaUtils;
-import it.govhub.rest.backoffice.utils.PostgreSQLUtilities;
 import it.govhub.rest.backoffice.utils.RequestUtils;
 
 
@@ -52,11 +44,6 @@ public class UserController implements UsersApi {
 	@Autowired
 	private UserAssembler userAssembler;
 	
-	@Autowired
-	private ObjectMapper objectMapper;
-	
-	@Autowired
-	private org.springframework.validation.Validator validator;
 
 	@Override
 	public ResponseEntity<User> readUser(Long id) {
@@ -101,56 +88,10 @@ public class UserController implements UsersApi {
 	@Override
 	public ResponseEntity<User> updateUser(Long id, List<PatchOp> patchOp) {
 		
-		UserEntity user = this.userRepo.findById(id)
-				.orElseThrow( () -> new ResourceNotFoundException("User with ID ["+id+"] not found"));
-		
 		// Otteniamo l'oggetto JsonPatch
 		JsonPatch patch = RequestUtils.toJsonPatch(patchOp);
 		
-		// Convertiamo la entity in json e applichiamo la patch sul json
-		User restUser = this.userAssembler.toModel(user);
-		JsonNode jsonUser = this.objectMapper.convertValue(restUser, JsonNode.class);
-		
-		JsonNode newJsonUser;
-		try {
-			newJsonUser = patch.apply(jsonUser);
-		} catch (JsonPatchException e) {			
-			throw new BadRequestException(e.getLocalizedMessage());
-		}
-		
-		// Lo converto nell'oggetto userCreate
-		UserCreate updatedContact;
-		try {
-			updatedContact = this.objectMapper.treeToValue(newJsonUser, UserCreate.class);
-		} catch (JsonProcessingException e) {
-			throw new BadRequestException(e);
-		}
-		
-		if (updatedContact == null) {
-			throw new BadRequestException("PATCH non valida, risulterebbe in un oggetto nullo.");
-		}
-		
-		// Faccio partire la validazione
-		Errors errors = new BeanPropertyBindingResult(updatedContact, updatedContact.getClass().getName());
-		validator.validate(updatedContact, errors);
-		if (errors != null && !errors.getAllErrors().isEmpty()) {
-			String msg = "L'oggetto patchato viola i vincoli dello schema: " + RequestUtils.extractValidationError(errors.getAllErrors().get(0));
-			throw new BadRequestException(msg);
-		}
-		
-		// Faccio partire la validazione custom per la stringa \u0000
-		PostgreSQLUtilities.throwIfContainsNullByte(updatedContact.getFullName(), "full_name");
-		
-		// Dall'oggetto REST passo alla entity
-		UserEntity newUser = UserEntity.builder()
-				.email(updatedContact.getEmail())
-				.enabled(updatedContact.isEnabled())
-				.full_name(updatedContact.getFullName())
-				.id(id)
-				.principal(updatedContact.getPrincipal())
-				.build();
-		
-		newUser = this.userRepo.save(newUser);
+		UserEntity newUser = this.userService.patchUser(id, patch);
 
 		// Dalla entity ripasso al model
 		User ret = this.userAssembler.toModel(newUser);
