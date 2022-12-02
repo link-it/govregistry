695eee6626ad03ba4afe630e81069da6e7c1a0c6
fscarlato <f.scarlato@protonmail.com>
2022-12-02 14:31:27 +0100

    Impl Autenticazione HeaderBased con filtro Spring.
    Gestite le eccezioni durante questo filtro spring.

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/security/GovhubUserDetailService.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/security/GovhubUserDetailService.java
@@ -4,30 +4,40 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Service;
 
 import it.govhub.rest.backoffice.config.Caches;
 import it.govhub.rest.backoffice.entity.UserEntity;
-import it.govhub.rest.backoffice.exception.ResourceNotFoundException;
+import it.govhub.rest.backoffice.exception.NotAuthorizedException;
 import it.govhub.rest.backoffice.repository.UserRepository;
 
-public class GovhubUserDetailService implements UserDetailsService {
+@Service
+public class GovhubUserDetailService implements UserDetailsService, AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
 	
 	@Autowired
 	private UserRepository userRepo;
 
-
+	
 	@Override
 	@Transactional
 	@Cacheable(Caches.PRINCIPALS)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		UserEntity user = this.userRepo.findAndPreloadByPrincipal(username)
-				.orElseThrow( () -> new ResourceNotFoundException("Credenziali non Valide"));
+				.orElseThrow( () -> new NotAuthorizedException("Credenziali non Valide"));
 		
 		return new GovhubPrincipal(user);
+	}
+
+
+	@Override
+	public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
+		return this.loadUserByUsername((String)token.getPrincipal());
 	}
 
 }
