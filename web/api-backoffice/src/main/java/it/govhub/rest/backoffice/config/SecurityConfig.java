0c58fe89420d921b48cc599772a49589e8f9ef9f
fscarlato <f.scarlato@protonmail.com>
2022-12-02 14:31:27 +0100

    Impl Autenticazione HeaderBased con filtro Spring.
    Gestite le eccezioni durante questo filtro spring.

--- web/api-backoffice/src/main/java/it/govhub/rest/backoffice/config/SecurityConfig.java
+++ web/api-backoffice/src/main/java/it/govhub/rest/backoffice/config/SecurityConfig.java
@@ -1,42 +1,36 @@
 package it.govhub.rest.backoffice.config;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.Set;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import it.govhub.rest.backoffice.exception.RestResponseEntityExceptionHandler;
-import it.govhub.rest.backoffice.exception.UnreachableException;
 import it.govhub.rest.backoffice.security.GovhubUserDetailService;
+import it.govhub.rest.backoffice.security.PreAuthenticatedExceptionHandler;
 import it.govhub.rest.backoffice.security.ProblemHttp403ForbiddenEntryPoint;
 
 
@@ -49,10 +43,14 @@ import it.govhub.rest.backoffice.security.ProblemHttp403ForbiddenEntryPoint;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
+	
 
 	@Autowired
 	private ObjectMapper jsonMapper;
+	
+    @Value("${auth.header}")
+    String headerAuthentication;
 	
 	public static final String REALM_NAME = "govhub";
 	public static final String JSESSIONID_NAME = "GOVHUB-JSESSIONID";
@@ -79,66 +77,43 @@ public class SecurityConfig {
 			);
 	
 	
+	@Autowired
+	public GovhubUserDetailService userDetailService;
+	
+    @Autowired
+    private PreAuthenticatedExceptionHandler preAuthenticatedExceptionHandler;
+	
 	@Bean
-	public SecurityFilterChain securityFilterChainDev(HttpSecurity http) throws Exception {
-		
-		//.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).
-		
-		applyAuthRules(http).csrf().disable()
-		/*.csrf()
-	    	.requireCsrfProtectionMatcher(new AndRequestMatcher(
-	    			CsrfFilter.DEFAULT_CSRF_MATCHER,
-	    			new RequestHeaderRequestMatcher("JSESSIONID")))
-		.and()*/
-				.formLogin()
-					.loginPage("/login")
-					.loginProcessingUrl("/login-do")
-					.failureUrl("/failure")
-					.permitAll()
-			.and().exceptionHandling()
-					.accessDeniedHandler(this.accessDeniedHandler())
-					.authenticationEntryPoint(new ProblemHttp403ForbiddenEntryPoint(this.jsonMapper));
-		return http.build();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 
-	private HttpSecurity applyAuthRules(HttpSecurity http) throws Exception {
-		
-		http
-		.authorizeRequests()
-		// richieste GET Schema open-api accessibile a tutti
-		.antMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll() 
-		.antMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
-	/*	.antMatchers(HttpMethod.POST, "/users/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_USERS_EDITOR)
-		.antMatchers(HttpMethod.PATCH, "/users/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_USERS_EDITOR)
-		.antMatchers(HttpMethod.GET, "/users/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_USERS_EDITOR, RUOLO_GOVHUB_USERS_VIEWER)
-		
-		.antMatchers(HttpMethod.GET, "/organizations/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_ORGANIZATIONS_EDITOR, RUOLO_GOVHUB_ORGANIZATIONS_VIEWER)
-		.antMatchers(HttpMethod.POST, "/organizations").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_ORGANIZATIONS_EDITOR)
-		.antMatchers(HttpMethod.PATCH, "/organizations/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_ORGANIZATIONS_EDITOR)
-		
-		.antMatchers(HttpMethod.DELETE,  "/authorizations/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_USERS_EDITOR)
-		
-		.antMatchers(HttpMethod.GET, "/services/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_SERVICE_EDITOR, RUOLO_GOVHUB_USERS_EDITOR)
-		.antMatchers(HttpMethod.POST, "/services").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_SERVICE_EDITOR)
-		.antMatchers(HttpMethod.PATCH, "/services/**").hasAnyRole(RUOLO_GOVHUB_SYSADMIN, RUOLO_GOVHUB_SERVICE_EDITOR)
-		
-		.antMatchers(HttpMethod.GET, "/profile").hasAnyRole(ruoliConsentiti.toArray(new String[0]))
-
-		.antMatchers(HttpMethod.GET, "/status").authenticated()
-		.antMatchers(HttpMethod.GET, "/error").authenticated()*/
-		.anyRequest().authenticated()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)  // Creazione della sessione in caso non ci sia
-		.and().logout().deleteCookies(JSESSIONID_NAME).invalidateHttpSession(true).logoutSuccessHandler(new DefaultLogoutSuccessHandler()) // Gestione Logout
-		;
-		return http;
-	}
-	
-	
 	@Bean
 	public AccessDeniedHandler accessDeniedHandler() {
 		return new AccessDeniedHandlerImpl(this.jsonMapper);
 	}
 	
+
+	@Bean
+	public SecurityFilterChain securityFilterChainDev(HttpSecurity http) throws Exception {
+		
+		AuthenticationManager manager = this.authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+		
+		RequestHeaderAuthenticationFilter filter = new RequestHeaderAuthenticationFilter();
+		filter.setPrincipalRequestHeader(this.headerAuthentication);
+		filter.setExceptionIfHeaderMissing(false);
+		filter.setAuthenticationManager(manager);
+		
+		applyAuthRules(http).csrf().disable()																												// Disabilita csrf perchè il token di sessione è SameSite: Strict
+		.addFilterBefore(filter, filter.getClass())																											// Autenticazione per header
+		.addFilterBefore(preAuthenticatedExceptionHandler, LogoutFilter.class)
+		.exceptionHandling()
+				.accessDeniedHandler(this.accessDeniedHandler())																		// Gestisci accessDenied in modo da restituire un problem ben formato
+				.authenticationEntryPoint(new ProblemHttp403ForbiddenEntryPoint(this.jsonMapper));		// Gestisci la mancata autenticazione con un problem ben formato
+		return http.build();
+	}
+
 	
 	@Bean
 	public CookieSerializer cookieSerializer() {
@@ -149,20 +124,35 @@ public class SecurityConfig {
 		return serializer;
 	}
 
-	
+
 	@Bean
-	public GovhubUserDetailService userDetailsService() {
-		return new GovhubUserDetailService();
+	public PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
+		PreAuthenticatedAuthenticationProvider ret = new PreAuthenticatedAuthenticationProvider();
+		ret.setPreAuthenticatedUserDetailsService(userDetailService);
+		return ret;
 	}
 	
 	
 	public class DefaultLogoutSuccessHandler implements LogoutSuccessHandler {
-
 	    @Override
 	    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 	            response.setStatus(HttpServletResponse.SC_OK);
 	    }
-
+	}
+	
+	
+	private HttpSecurity applyAuthRules(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeRequests()
+		// richieste GET Schema open-api accessibile a tutti
+		.antMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll() 
+		.antMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
+		.anyRequest().authenticated()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)  // Creazione della sessione in caso non ci sia
+		.and().logout().deleteCookies(JSESSIONID_NAME).invalidateHttpSession(true).logoutSuccessHandler(new DefaultLogoutSuccessHandler()) // Gestione Logout
+		;
+		return http;
 	}
 	
 }
