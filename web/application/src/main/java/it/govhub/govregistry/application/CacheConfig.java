6edf68b691e061be14f60e946db7859bd298ea21
fscarlato <f.scarlato@protonmail.com>
2022-12-13 02:38:42 +0100

    Adesso la cache viene abilitata dall'applicazione e non dal modulo comune, che comunque ne fa uso, ma non l'attiva.

---
+++ web/application/src/main/java/it/govhub/govregistry/application/CacheConfig.java
@@ -0,0 +1,9 @@
+package it.govhub.govregistry.application;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {	
+}
