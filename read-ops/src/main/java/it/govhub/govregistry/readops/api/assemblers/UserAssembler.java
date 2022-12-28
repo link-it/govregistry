769f7de37b20430eed67739b9698f8f6ce311ffe
Giuliano Pintori <pintori@link.it>
2022-12-28 19:25:33 +0100

    Risolte segnalazioni code-smell

--- read-ops/src/main/java/it/govhub/govregistry/readops/api/assemblers/UserAssembler.java
+++ read-ops/src/main/java/it/govhub/govregistry/readops/api/assemblers/UserAssembler.java
@@ -3,16 +3,10 @@ package it.govhub.govregistry.readops.api.assemblers;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
-import it.govhub.govregistry.commons.api.beans.Authorization;
-import it.govhub.govregistry.commons.api.beans.Profile;
 import it.govhub.govregistry.commons.api.beans.User;
 import it.govhub.govregistry.commons.api.beans.UserCreate;
 import it.govhub.govregistry.commons.entity.UserEntity;
