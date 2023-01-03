974c72140617e3bf0dc414af8026457fba0aaba4
fscarlato <f.scarlato@protonmail.com>
2023-01-03 13:28:13 +0100

    Cambiata la classe di utility 'Base64' usata dalla Base64String.java. In questo modo in fase di costruzione dell'oggetto REST viene validata correttamente

--- read-ops/src/main/java/it/govhub/govregistry/readops/api/assemblers/OrganizationAssembler.java
+++ read-ops/src/main/java/it/govhub/govregistry/readops/api/assemblers/OrganizationAssembler.java
@@ -52,11 +52,11 @@ public class OrganizationAssembler extends RepresentationModelAssemblerSupport<O
 		BeanUtils.copyProperties(src,  ret);
 		
 		if (src.getLogo() != null) {
-			ret.setLogo(Base64.decodeBase64(src.getLogo().getValue()));
+			ret.setLogo(src.getLogo().getDecodedValue());
 		}
 		
 		if (src.getLogoMiniature() != null) {
-			ret.setLogoMiniature(Base64.decodeBase64(src.getLogoMiniature().getValue()));
+			ret.setLogoMiniature(src.getLogoMiniature().getDecodedValue());
 		}
 		
 		return ret;
