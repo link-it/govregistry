f3ac26841cfc74822bf0ffc6ef6bfbdcc1db3eb0
fscarlato <f.scarlato@protonmail.com>
2023-01-26 14:38:42 +0100

    Rimossa la GET /users/{id}/authorization dalla parte comune
    Rimossa la generazione dei beans dell'openapi da read-ops

--- api/src/main/java/it/govhub/govregistry/api/web/AuthorizationController.java
+++ api/src/main/java/it/govhub/govregistry/api/web/AuthorizationController.java
@@ -1,6 +1,8 @@
 package it.govhub.govregistry.api.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -8,10 +10,17 @@ import it.govhub.govregistry.api.beans.AuthorizationCreate;
 import it.govhub.govregistry.api.services.RoleAuthorizationService;
 import it.govhub.govregistry.api.spec.AuthorizationApi;
 import it.govhub.govregistry.commons.api.beans.Authorization;
+import it.govhub.govregistry.commons.api.beans.AuthorizationList;
+import it.govhub.govregistry.commons.api.beans.AuthorizationOrdering;
 import it.govhub.govregistry.commons.config.V1RestController;
-import it.govhub.govregistry.readops.api.assemblers.AuthorizationAssembler;
+import it.govhub.govregistry.commons.entity.RoleAuthorizationEntity_;
+import it.govhub.govregistry.commons.entity.RoleEntity_;
+import it.govhub.govregistry.commons.exception.UnreachableException;
+import it.govhub.govregistry.commons.utils.LimitOffsetPageRequest;
+import it.govhub.govregistry.readops.api.assemblers.AuthorizationConverter;
 import it.govhub.govregistry.readops.api.repository.ReadRoleAuthorizationRepository;
 import it.govhub.govregistry.readops.api.repository.ReadRoleRepository;
+import it.govhub.security.config.GovregistryRoles;
 import it.govhub.security.services.SecurityService;
 
 
@@ -19,7 +28,7 @@ import it.govhub.security.services.SecurityService;
 public class AuthorizationController implements AuthorizationApi {
 	
 	@Autowired
-	AuthorizationAssembler authAssembler;
+	AuthorizationConverter authAssembler;
 	
 	@Autowired
 	RoleAuthorizationService authService;
@@ -45,6 +54,34 @@ public class AuthorizationController implements AuthorizationApi {
 		this.authService.removeAuthorization(authId);
 		
 		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	
+	@Override
+	public ResponseEntity<AuthorizationList> listAuthorizations(Long userId, AuthorizationOrdering sort,  Direction sortDirection, Integer limit, Long offset) {
+		
+		this.securityService.expectAnyRole(GovregistryRoles.GOVREGISTRY_SYSADMIN, GovregistryRoles.GOVREGISTRY_USERS_EDITOR, GovregistryRoles.GOVREGISTRY_USERS_VIEWER);
+		
+		Sort orderBy;
+		switch(sort) {
+			case ID:
+				orderBy = Sort.by(sortDirection, RoleAuthorizationEntity_.ID);
+				break;
+			case ROLE_NAME:
+				orderBy = Sort.by(sortDirection, RoleAuthorizationEntity_.ROLE+"."+RoleEntity_.NAME);
+				break;
+			case UNSORTED:
+				orderBy = Sort.unsorted();
+				break;
+			default:
+				throw new UnreachableException();
+		}
+		
+		LimitOffsetPageRequest pageRequest = new LimitOffsetPageRequest(offset, limit, orderBy);
+		
+		AuthorizationList ret = authService.listUserAuthorizations(userId, pageRequest);
+		
+		return ResponseEntity.ok(ret);
 	}
 
 
