4df6e0a09d743b70355c4b92b9f09bd62ada5d1f
Alfonso Tatarelli <a.tatarelli@gmail.com>
2023-03-14 09:48:30 +0100

    UI: Aggiornato componente "SearchBarForm"

--- webapp/projects/components/src/lib/ui/search-bar-form/search-bar-form.module.ts
+++ webapp/projects/components/src/lib/ui/search-bar-form/search-bar-form.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { MatIconModule } from '@angular/material/icon';
 
 import { TranslateModule } from '@ngx-translate/core';
@@ -12,6 +14,7 @@ import { SearchBarFormComponent } from './search-bar-form.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MatIconModule,
     TranslateModule
   ],
