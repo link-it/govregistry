82f0557d484f1727caa4c1af0655b1b59f2a87e6
Alfonso Tatarelli <a.tatarelli@gmail.com>
2023-03-20 22:21:23 +0100

    Component "ItemType": aggiunto valore di default da config

--- webapp/projects/components/src/lib/ui/item-type/item-type.component.ts
+++ webapp/projects/components/src/lib/ui/item-type/item-type.component.ts
@@ -51,7 +51,7 @@ export class ItemTypeComponent implements OnInit, AfterViewInit {
   ) { }
 
   ngOnInit() {
-    this._value = this.utilsLib.getObjectValue(this._data.source, this._elem.field);
+    this._value = this.utilsLib.getObjectValue(this._data.source, this._elem.field) || (this._elem.default || '');
     if (this._elem.type === 'status') {
       if (this._config.options) {
         const _origValue = this._value;
