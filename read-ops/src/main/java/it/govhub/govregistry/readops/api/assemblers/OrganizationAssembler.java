00638cf89458d7637e72bf5a55a4e39b4eedbc8d
fscarlato <f.scarlato@protonmail.com>
2023-03-21 11:01:32 +0100

    Adesso i links riportano correttamente il path del controller /v1

--- read-ops/src/main/java/it/govhub/govregistry/readops/api/assemblers/OrganizationAssembler.java
+++ read-ops/src/main/java/it/govhub/govregistry/readops/api/assemblers/OrganizationAssembler.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 import it.govhub.govregistry.commons.api.beans.Organization;
 import it.govhub.govregistry.commons.api.beans.OrganizationCreate;
 import it.govhub.govregistry.commons.entity.OrganizationEntity;
-import it.govhub.govregistry.readops.api.spec.OrganizationApi;
+import it.govhub.govregistry.readops.api.web.ReadOrganizationController;
 
 @Component
 public class OrganizationAssembler extends RepresentationModelAssemblerSupport<OrganizationEntity, Organization> {
@@ -20,7 +20,7 @@ public class OrganizationAssembler extends RepresentationModelAssemblerSupport<O
 	Logger log = LoggerFactory.getLogger(OrganizationAssembler.class);
 
 	public OrganizationAssembler() {
-		super(OrganizationApi.class, Organization.class);
+		super(ReadOrganizationController.class, Organization.class);
 	}
 	
 
@@ -32,21 +32,21 @@ public class OrganizationAssembler extends RepresentationModelAssemblerSupport<O
 		BeanUtils.copyProperties(src, ret);
 		
 		ret.add(linkTo(
-				methodOn(OrganizationApi.class)
+				methodOn(ReadOrganizationController.class)
 				.readOrganization(src.getId()))
 			.withSelfRel()
 		);
 		
 		if(src.getLogo() != null) {
 			ret.add(linkTo(
-					methodOn(OrganizationApi.class)
+					methodOn(ReadOrganizationController.class)
 					.downloadOrganizationLogo(src.getId()))
 				.withRel("logo"));
 		}
 		
 		if (src.getLogoMiniature() != null) {
 			ret.add(linkTo(
-						methodOn(OrganizationApi.class)
+						methodOn(ReadOrganizationController.class)
 						.downloadOrganizationLogoMiniature(src.getId()))
 				.withRel("logo-miniature"));
 		}
