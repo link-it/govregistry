764433503d4acfc51881d43addce1802cdd8ea03
fscarlato <f.scarlato@protonmail.com>
2023-05-31 14:15:34 +0200

    minor changes

--- commons/src/main/java/it/govhub/govregistry/commons/security/AccessDeniedHandlerImpl.java
+++ commons/src/main/java/it/govhub/govregistry/commons/security/AccessDeniedHandlerImpl.java
@@ -34,7 +34,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -46,7 +45,7 @@ import it.govhub.govregistry.commons.exception.handlers.RestResponseEntityExcept
 * Handler che viene eseguito quando un oggetto {@link org.springframework.security.core.Authentication Authentication} non ha
 * l'autorizzazione richiesta. (e.g.: visistare un'endpoint) 
 */
-@Component
+
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
 
 	@Autowired
